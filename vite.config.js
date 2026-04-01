1ccb8956096611a9db7e220bf9c19a5028b60d4a
iamshaoning <825824748@qq.com>
2026-04-02 01:14:14 +0800

    修复环境变量加载方式

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
-export default defineConfig({
-  base: './',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  },
-  publicDir: 'public',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
-          VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-          VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
-          VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-          VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-          VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-          VITE_FIREBASE_DATABASE_URL: process.env.VITE_FIREBASE_DATABASE_URL
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  
+  return {
+    base: './',
+    build: {
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name]-[hash].[ext]'
         }
       }
-    })
-  ]
+    },
+    publicDir: 'public',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            VITE_FIREBASE_API_KEY: env.VITE_FIREBASE_API_KEY,
+            VITE_FIREBASE_AUTH_DOMAIN: env.VITE_FIREBASE_AUTH_DOMAIN,
+            VITE_FIREBASE_PROJECT_ID: env.VITE_FIREBASE_PROJECT_ID,
+            VITE_FIREBASE_STORAGE_BUCKET: env.VITE_FIREBASE_STORAGE_BUCKET,
+            VITE_FIREBASE_MESSAGING_SENDER_ID: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+            VITE_FIREBASE_APP_ID: env.VITE_FIREBASE_APP_ID,
+            VITE_FIREBASE_DATABASE_URL: env.VITE_FIREBASE_DATABASE_URL
+          }
+        }
+      })
+    ]
+  }
 })
