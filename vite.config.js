4e084bf76f17567c7808d48a252cc0e10ee68263
iamshaoning <825824748@qq.com>
2026-04-02 01:11:20 +0800

    修复GitHub Actions环境变量注入

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   base: './',
@@ -16,5 +17,20 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
-  }
+  },
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+          VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+          VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+          VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+          VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+          VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+          VITE_FIREBASE_DATABASE_URL: process.env.VITE_FIREBASE_DATABASE_URL
+        }
+      }
+    })
+  ]
 })
