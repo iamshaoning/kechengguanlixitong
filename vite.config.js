9d47b94e99e3a22063f4cce0d09a408395010105
iamshaoning <825824748@qq.com>
2026-04-01 22:53:07 +0800

    Fix logo path

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
+})
